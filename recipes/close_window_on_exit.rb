7e4b035c7392e1f344e88e932ee9f8bd2989b5ab
Jonathan Barnes <jonathan@codeword.net>
2013-11-12 20:07:28 -0800

    Adds shellExitAction key incase it is not there

--- recipes/close_window_on_exit.rb
+++ recipes/close_window_on_exit.rb
@@ -1,6 +1,7 @@
 include_recipe 'sprout-terminal::check_for_active'
 
 execute "Closes window when shell exits cleanly the #{node['terminal']['profile']} profile" do
-  command "/usr/libexec/PlistBuddy -c 'set :Window\\ Settings:#{node['terminal']['profile']}:shellExitAction 1' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
+  command "/usr/libexec/PlistBuddy -c 'add :Window\\ Settings:#{node['terminal']['profile']}:shellExitAction integer 1' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist; /usr/libexec/PlistBuddy -c 'set :Window\\ Settings:#{node['terminal']['profile']}:shellExitAction 1' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
+  ignore_failure true
   user node['current_user']
 end
