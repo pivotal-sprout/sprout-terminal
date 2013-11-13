9e9fb339658ba0f2b68cdecc20af51ad8013d673
Jonathan Barnes <jbarnes@wifi-2-49.sf.pivotallabs.com>
2013-11-12 19:07:50 -0800

    Fixes warning when terminal is running
    It now references the cookbook instead of the current file.

--- recipes/check_for_active.rb
+++ recipes/check_for_active.rb
@@ -1,3 +1,3 @@
 if (system("ps auxwww | grep -i terminal.app |grep -v grep"))
-  Chef::Log.warn "\033[31m The '#{File.basename(__FILE__)}' recipe works inconsistently while Terminal is running\033[39m"
+  Chef::Log.warn "\033[31m The 'sprout-terminal' cookbook works inconsistently while Terminal is running\033[39m"
 end
