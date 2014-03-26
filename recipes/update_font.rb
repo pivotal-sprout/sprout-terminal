9044cfd1affb857c21efaf2562dfb90464a3c944
Brian Cunnie and Jonathan Barnes <pair+cunnie+jonathan@pivotallabs.com>
2014-03-25 21:47:37 -0700

    Removes Menlo hard-coding from everywhere but the default attributes file.

--- recipes/update_font.rb
+++ recipes/update_font.rb
@@ -1,15 +1,15 @@
 include_recipe 'sprout-terminal::check_for_active'
 term_opts = node['sprout']['terminal']
 
-remote_file "#{Chef::Config[:file_cache_path]}/MenloRegular18pt.bin" do
+remote_file "#{Chef::Config[:file_cache_path]}/customFont.bin" do
   source term_opts['font_src']
   owner node['current_user']
   not_if { term_opts['default_profile'].nil? && term_opts['font_src'].nil? }
   only_if { term_opts['update_font'] }
 end
 
-execute "Updates font on the #{term_opts['default_profile']} profile to Menlo-Regular 18pt" do
-  command "/usr/libexec/PlistBuddy -c 'import :Window\\ Settings:#{term_opts['default_profile']}:Font #{Chef::Config[:file_cache_path]}/MenloRegular18pt.bin' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
+execute "Updates font on the #{term_opts['default_profile']} profile to #{term_opts['font_src']}" do
+  command "/usr/libexec/PlistBuddy -c 'import :Window\\ Settings:#{term_opts['default_profile']}:Font #{Chef::Config[:file_cache_path]}/customFont.bin' #{ENV['HOME']}/Library/Preferences/com.apple.Terminal.plist"
   user node['current_user']
   not_if { term_opts['default_profile'].nil? && term_opts['font_src'].nil? }
   only_if { term_opts['update_font'] }
